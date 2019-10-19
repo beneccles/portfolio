c07e280ca4d8f0c75f025aa446904385374d3709
Ben Eccles <ben.g.eccles@icloud.com>
2019-10-19 17:28:00 -0600

    removed particle packages for now. WHile very cool, I'm concern about it eating up gpu space for anyone using the site.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import ParticleAnimation from 'react-particle-animation';
 import Header from './components/Header/Header';
-import Particles from 'react-particles-js';
 import './App.css';
 
 function App() {
-  const particlesOptions = {
-    particles: {
-      number: {
-        value: 300,
-        density: {
-          enable: true,
-          value_area:800
-        }
-      }
-    }
-  }
+
   return (
     <div className="App">
       {/* <svg id="svgBack" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
@@ -26,8 +14,6 @@ function App() {
           <circle stroke="blue" cx="40" cy="40" r="5" />
         </g>
       </svg> */}
-      {/* <ParticleAnimation background={{ r:37, g:52, b:72, a:1 }} numParticles={800} /> */}
-      {/* <Particles className="particles" params={particlesOptions} /> */}
       <Header />
     </div>
   );
