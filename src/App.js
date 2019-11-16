e8f199d6ff44889944535814e5c8e98f184fb58e
Ben Eccles <ben.g.eccles@icloud.com>
2019-11-15 21:02:16 -0700

    clean up of Hero component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React, {useRef, useEffect, Component} from 'react';
-import Header from './components/Header/Header';
+import React, {Component} from 'react';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 import Skills from './components/Skills/Skills';
 import About from './components/About/About';
 import nasa from "./assets/nasa3.jpg";
-// eslint-disable-next-line
 import Hero from './components/Hero/Hero';
-// import { slide as Menu } from 'react-burger-menu'
 import './App.scss';
 
 
@@ -24,7 +21,6 @@ class App extends Component {
     return (
         <body>
       <div className="App" style={{ backgroundImage: `url(${nasa})` }}>
-        {/* <Header project={this.projects}/> */}
         <Hero />
         <About />
         <Projects ref={this.projects} />
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,31 +1,13 @@
-import React, {useRef} from "react";
-import me from "../../assets/ben_nobackground.png";
-import nasa from "../../assets/nasa2.jpg";
+import React from "react";
 import "./Hero.scss";
 
 function Hero() {
-    const projectLink = useRef(null);
-// style={{ backgroundImage: `url(${nasa})` }}
   return (
     <div id="Hero" >
-    <div id="satNav">
-      {/* <a href="#About">
-          <p id="pAbout" className="navLink">About</p>
-      </a>
-      <a className="menu-item" href="#Projects" ref={projectLink}>
-        <p id="pProjects" className="navLink">Projects</p>
-      </a>
-      <a className="Menu-item" href="#Skills">
-        <p id="pStack" className="navLink">Stack</p>
-      </a>
-      <a className="menu-item" href="#Contact">
-        <p id="pContact" className="navLink">Contact</p>
-      </a> */}
-    </div>
-  
       <div className="heroText">
         <p><a href="#About">About</a>   -   <a href="#Projects">Projects</a>   -   <a href="#Skills">Stack</a>   -   <a href="#Contact">Contact</a></p>
         <h1>My Name is Benjamin Eccles</h1>
+        <br/>
         <p>a Full Stack Developer based out of Lehi, UT.</p>
       </div>
     </div>
